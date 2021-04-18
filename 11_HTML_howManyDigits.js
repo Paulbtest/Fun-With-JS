be0b95f1b1f2351c6fda4c6bdf545354138623d4
PaulBTest <buchkovpv@yandex.ru>
2021-04-18 17:12:37 +0300

    Решение задачи "Сколько цифр?" с HTML Academy (Читерский способ).

---
+++ 11_HTML_howManyDigits.js
@@ -0,0 +1,18 @@
+// HTML Academy, испытание: сколько цифр
+
+let number = 123;
+let quantity = 0;
+let mediator = String(number);
+
+quantity = Number(mediator.length);
+console.log(quantity)
+
+/* Техническое задание
+
+Напиши программу, которая определяет сколько цифр в одном числе.
+
+Само число записано в переменную number.
+
+Найди количество цифр в этом числе и запиши результат в переменную quantity.
+
+*/
