fe424e9a4ad148e0729c4d156e57ac417b9dfbda
PaulBTest <buchkovpv@yandex.ru>
2021-04-30 21:48:52 +0300

    Добавлено решение задачи "Variable Swapping" из книги The JS Way

---
+++ The JS Way/chapter02/js/05_variableSwapping.js
@@ -0,0 +1,23 @@
+let number1 = 5;
+let number2 = 3;
+let swapper;
+
+swapper = number1; // Stores the value of number1
+number1 = number2;
+number2 = swapper;
+
+console.log(`The value of number1 = ${number1}`);
+console.log(`The value of number2 = ${number2}`);
+
+/*
+Variable swapping
+Observe the following program.
+
+let number1 = 5;
+let number2 = 3;
+// TODO: type your code here (and nowhere else!)
+
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
+Add the necessary code to swap the values of variables number1 and number2.
+*/
