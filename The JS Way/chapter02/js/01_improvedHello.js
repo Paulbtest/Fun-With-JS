bdfec91d19dd1ef3a2d158e84dd665bd98ecca89
PaulBTest <buchkovpv@yandex.ru>
2021-04-30 20:30:23 +0300

    Добавлено решение задачи "Improved Hello" из книги The JS Way

---
+++ The JS Way/chapter02/js/01_improvedHello.js
@@ -0,0 +1,9 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+
+alert(`Hello ${firstName} ${lastName}, nice to meet you!`);
+
+/* 
+Write a program that asks the user for his first name and his last name. The program then displays
+them in one sentence.
+*/
