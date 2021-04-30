10abe9bcfc423395a3f645802809a6b9b2ff19de
PaulBTest <buchkovpv@yandex.ru>
2021-04-30 21:27:53 +0300

    Добавлено решение задачи "From Celsius to Fahrenheit Degrees" из книги The JS Way

---
+++ The JS Way/chapter02/js/04_fromCelsiusToFahrenheitDegrees.js
@@ -0,0 +1,8 @@
+let tempCelsius = Number(prompt("What is the temperature in Celsius degrees?"));
+let tempFahrenheit = tempCelsius * 9 / 5 + 32;
+
+console.log(`The temperature in Fahrenheit degrees is ${tempFahrenheit}`)
+
+/* Write a program that asks for a temperature in Celsius degrees, 
+then displays it in Fahrenheit degrees.
+The conversion between scales is given by the formula: [°F] = [°C] x 9 / 5 + 32. */
