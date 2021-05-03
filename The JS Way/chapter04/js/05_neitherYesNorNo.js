eb565c599e576d85dcfdc7bd73b45968650e9afc
PaulBTest <buchkovpv@yandex.ru>
2021-05-03 21:34:09 +0300

    Решение задачи "Neither yes nor no" из книги The JS Way

---
+++ The JS Way/chapter04/js/05_neitherYesNorNo.js
@@ -0,0 +1,10 @@
+let userText = '';
+
+while (userText !== "yes" && userText !== "no") {
+    
+    userText = prompt("Are you tired of typing text inside this box?").toLowerCase();
+};
+
+
+/* Write a program that plays “neither yes, nor no” with the user.Specifically, the programs asks
+the user to enter text until either “yes” or “no” is typed, which ends the game. */
