14c80c5113f34ba3777920291be6bbcdffb57c69
PaulBTest <buchkovpv@yandex.ru>
2021-05-03 20:50:26 +0300

    Решение задачи "Multiplication Table" из книги The JS Way

---
+++ The JS Way/chapter04/js/04_multiplicationTable.js
@@ -0,0 +1,17 @@
+let userNumber = 0;
+
+
+while (userNumber < 2 || userNumber > 9 || isNaN(userNumber)) {
+        userNumber = Number(prompt("Enter a number between 2 and 9:"));
+    };
+
+    for (let i = 0; i <= 10; i++) {
+        
+        console.log(`${userNumber} * ${i} = ${userNumber * i}`);
+    };
+
+
+/* Write a program that asks the user for a number, then shows the multiplication table for this
+number.
+When you are done, improve the program so it only accepts numbers between 2 and 9(use the
+previous exercise as a blueprint). */
