609334aa0090056fdfa9a7996b91cc48c67dc088
PaulBTest <buchkovpv@yandex.ru>
2021-05-06 22:37:13 +0300

    Решение седьмой задачи из контрольных вопросов

---
+++ Control Questions/js/question07.js
@@ -0,0 +1,10 @@
+let str = "";
+
+for (let i = 1; i <= 9; i++) {
+    str += String(i);
+    console.log(str)
+};
+
+console.log(str);
+
+// С помощью цикла for сформируйте строку '123456789' и запишите ее в переменную str.
