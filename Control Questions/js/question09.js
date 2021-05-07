57de44b896f519c2e705dcb48bb9e2104dfe7e1e
PaulBTest <buchkovpv@yandex.ru>
2021-05-07 19:38:04 +0300

    Решение девятой задачи из контрольных вопросов

---
+++ Control Questions/js/question09.js
@@ -0,0 +1,10 @@
+let arrHelloWorld = ['Привет, ', 'мир', '!'];
+let finalString = "";
+
+for (i = 0; i < arrHelloWorld.length; i++) {
+    finalString += arrHelloWorld[i];
+};
+
+console.log(finalString);
+
+// Дан массив с элементами 'Привет, ', 'мир' и '!'.Необходимо вывести на экран фразу 'Привет, мир!'.
