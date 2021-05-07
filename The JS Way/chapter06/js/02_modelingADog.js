13a84810a74a0d45d4d6f1ae0e6380325572b994
PaulBTest <buchkovpv@yandex.ru>
2021-05-07 18:29:23 +0300

    Решение задачи "Modeling a Dog" из книги The JS Way.

---
+++ The JS Way/chapter06/js/02_modelingADog.js
@@ -0,0 +1,16 @@
+// TODO: create the dog object here
+
+const dog = {
+    name: "Larry",
+    species: "retriver",
+    size: 100,
+
+    bark() {
+        return "Grrr-Grrrr!"
+    }
+};
+
+console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`);
+
+// Complete the following program to add the dog object definition.
