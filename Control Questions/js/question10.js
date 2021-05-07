fed4b83b8492811e182386272369f4f2d3e59e25
PaulBTest <buchkovpv@yandex.ru>
2021-05-07 19:47:22 +0300

    Решение десятой задачи из контрольных вопросов

---
+++ Control Questions/js/question10.js
@@ -0,0 +1,10 @@
+let arrLetters = ['a', 'b', 'c'];
+let arrNumbers = [1, 2, 3];
+
+for (let i = 0; i < arrNumbers.length; i++) {
+    arrLetters.push(arrNumbers[i]);
+}
+
+console.log(arrLetters);
+
+// Даны два массива: ['a', 'b', 'c'] и [1, 2, 3]. Объедините их вместе.
