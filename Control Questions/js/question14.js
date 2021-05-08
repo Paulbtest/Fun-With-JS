744d1c4c70fa887680ff6d32d529538d989ef631
PaulBTest <buchkovpv@yandex.ru>
2021-05-08 21:05:14 +0300

    Решение четырнадцатой задачи из контрольных вопросов

---
+++ Control Questions/js/question14.js
@@ -0,0 +1,12 @@
+let x = Number(prompt("Введите число X."));
+let secondNumber = Number(prompt("Введите второе число"));
+
+let oneThirdOfX = Math.round(x * 0.333);
+
+secondNumber += oneThirdOfX;
+console.log(secondNumber)
+
+
+/* Напишите фрагмент кода, который присваивает переменной х значение типа Number, а
+затем присваивает переменной треть от значения переменной х.Выводить результат на
+экран. */
