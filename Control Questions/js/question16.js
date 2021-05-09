153549ed06790eaa48570c9cd33776a01cab8967
PaulBTest <buchkovpv@yandex.ru>
2021-05-09 22:19:04 +0300

    Исправление ошибки в шестнадцатой задаче

--- Control Questions/js/question16.js
+++ Control Questions/js/question16.js
@@ -1,5 +1,5 @@
 let userNumber = Number(prompt("Введите число."));
-console.log(`${userNumber} * 10 = "${userNumber * 10}"`);
+console.log(`${userNumber} * 10 = "${userNumber + 10}"`);
 
 /* Напишите фрагмент кода, который принимает от пользователя число и выводит на экран
 число, которое больше введенного с клавиатуры на 10.
