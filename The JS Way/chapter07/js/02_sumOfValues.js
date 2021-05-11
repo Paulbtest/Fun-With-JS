86070c70f0437697d8db9f49e7acab1dae5cda9b
PaulBTest <buchkovpv@yandex.ru>
2021-05-11 16:42:45 +0300

    Решение задачи "Sum of Values" из книги The JS Way

---
+++ The JS Way/chapter07/js/02_sumOfValues.js
@@ -0,0 +1,11 @@
+const values = [3, 11, 7, 2, 9, 10];
+let sumOfValues = 0;
+
+for (let i = 0; i < values.length; i++) {
+    sumOfValues += values[i];
+};
+
+console.log(sumOfValues);
+
+/* Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case).
+const values = [3, 11, 7, 2, 9, 10]; */
