5d665ff83d038fd422ffc6566e2aae31e65ea3c5
PaulBTest <buchkovpv@yandex.ru>
2021-05-14 19:26:17 +0300

    Решение задачи "Word Info" из книги The JS way

---
+++ The JS Way/chapter08/js/01_wordInfo.js
@@ -0,0 +1,9 @@
+let userWord = prompt("Please enter a word: ");
+
+console.log(userWord.length);
+console.log(userWord.toLowerCase());
+console.log(userWord.toUpperCase());
+
+
+
+/* Write a program that asks you for a word then shows its length, lowercase, and uppercase values. */
