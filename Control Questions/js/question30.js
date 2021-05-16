7afe020abbb84c66a2813428ab56a5dcf4fdb1ce
PaulBTest <buchkovpv@yandex.ru>
2021-05-16 14:03:37 +0300

    Решение тридцатой задачи из контрольных вопросов

---
+++ Control Questions/js/question30.js
@@ -0,0 +1,14 @@
+let userNumber = Number(prompt("Введите целое положительное двузначное число: "));
+
+if (isNaN(userNumber)) {
+    console.log("Ошибка. Введеное значение не является числом.")
+} else if (userNumber < 0 || userNumber <= 99 || userNumber > 999 || !Number.isInteger(userNumber)) {
+    console.log("Ошибка. Введеное значение не является целым, положительным или трехзначным числом.")
+} else {
+    userNumber = String(userNumber);
+    console.log(userNumber[0] + "00" + " " + userNumber[1] + "0" + " " + userNumber[2]);
+};
+
+/* Напишите код, который принимает с клавиатуры целое положительное трехзначное число и
+выводит его на экран в «полном виде»: например, для числа 364 это будет выглядеть как
+300 60 4. */
