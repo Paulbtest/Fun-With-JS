485fbeb1e044cd54d82807983f2cbe2553d20a66
PaulBTest <buchkovpv@yandex.ru>
2021-05-16 13:05:09 +0300

    Решение двадцать седьмой задачи из контрольных вопросов

---
+++ Control Questions/js/question27.js
@@ -0,0 +1,14 @@
+let userNumber = Number(prompt("Введите целое положительное двузначное число: "));
+
+if (isNaN(userNumber)) {
+    console.log("Ошибка. Введеное значение не является числом.")
+} else if (userNumber < 0 || userNumber <= 99 || userNumber > 999 || !Number.isInteger(userNumber)) {
+    console.log("Ошибка. Введеное значение не является целым, положительным или трехзначным числом.")
+} else {
+    userNumber = String(userNumber);
+    console.log(userNumber[0] + " " + userNumber[1] + " " + userNumber[2]);
+};
+
+
+/* Напишите код, который принимает с клавиатуры целое положительное трехзначное число и
+выводит на экран его цифры, разделенные знаком «пробел». */
