44cb3a9912c8ccf26d87be183c6774480de16266
PaulBTest <buchkovpv@yandex.ru>
2021-05-17 18:28:38 +0300

    Решение тридцать шестой задачи из контрольных вопросов

---
+++ Control Questions/js/question36.js
@@ -0,0 +1,12 @@
+let array = [];
+let content = "";
+
+for (let i = 1; i <= 10; i++) {
+    content += "x";
+    array.push(content);
+};
+
+console.log(array);
+
+/* Заполните массив следующим образом: в первый элемент запишите 'x', во второй 'xx', в
+третий 'xxx' и так далее. */
