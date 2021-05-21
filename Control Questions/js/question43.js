5723535f6a9ef0ecdf857d5571f58b0edff8bf1a
PaulBTest <buchkovpv@yandex.ru>
2021-05-21 17:59:45 +0300

    Решение сорок третьей задачи из контрольных вопросов.

---
+++ Control Questions/js/question43.js
@@ -0,0 +1,7 @@
+let arrNumbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arrNumbers.length; i++) {
+    console.log(arrNumbers[i]);
+};
+
+// Дан массив с элементами[1, 2, 3, 4, 5].С помощью цикла for выведите все эти элементы на экран.
