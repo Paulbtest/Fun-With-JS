9048de5d72e9829f20cc5ca5a887c5480fe59e15
PaulBTest <buchkovpv@yandex.ru>
2021-05-23 16:51:58 +0300

    Решение сорок восьмой задачи из контрольных вопросов.

---
+++ Control Questions/js/question48.js
@@ -0,0 +1,9 @@
+let userNumber = Number(prompt("Введите число: "));
+
+let calculateSquare = function(number) {
+    return number ** 2;
+};
+
+console.log(calculateSquare(userNumber));
+
+//Сделайте функцию, которая возвращает квадрат числа. Число передается параметром.
