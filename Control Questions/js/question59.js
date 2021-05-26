129b74ba5d596c8bf26f5d799eb5ba6c0052e259
PaulBTest <buchkovpv@yandex.ru>
2021-05-26 21:02:43 +0300

    Решение пятьдесят девятой задачи из контрольных вопросов

---
+++ Control Questions/js/question59.js
@@ -0,0 +1,10 @@
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+for (let i = 0; i < arr2.length; i++) {
+    arr1.push(arr2[i]);
+};
+
+console.log(arr1);
+
+/* Даны два массива: [1, 2, 3] и [4, 5, 6].Объедините их вместе */
