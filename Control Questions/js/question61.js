ce3e26e9e7e6ee95ea564de7bfb48b2eaf659be4
PaulBTest <buchkovpv@yandex.ru>
2021-05-26 21:29:17 +0300

    Решение шестьдесят первой задачи из контрольных вопросов

---
+++ Control Questions/js/question61.js
@@ -0,0 +1,10 @@
+let firstNumber = Number(prompt("Введите первое число: "));
+let secondNumber = Number(prompt("Введите второе число: "));
+
+let calculateNumbers = function(numberA, numberB) {
+    return numberA + numberB;
+};
+
+console.log(calculateNumbers(firstNumber, secondNumber));
+
+// Сделайте функцию, которая возвращает сумму двух чисел.
