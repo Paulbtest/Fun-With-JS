9e02df32a9dfe09052487625ea2c9cde131d12db
PaulBTest <buchkovpv@yandex.ru>
2021-05-26 19:53:00 +0300

    Решение пятьдесят восьмой задачи из контрольных вопросов

---
+++ Control Questions/js/question58.js
@@ -0,0 +1,13 @@
+let arrayNumbers = [2, 3, 4, 5];
+let numberProduct = 1;
+
+for (let i = 0; i < arrayNumbers.length; i++) {
+    
+    numberProduct *= arrayNumbers[i];
+};
+
+console.log(numberProduct);
+
+/* Дан массив с элементами[2, 3, 4, 5].С помощью цикла for найдите произведение элементов
+этого массива.
+ */
