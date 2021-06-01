66b606d7263a5096cd53f7e79d0d15de97175294
PaulBTest <buchkovpv@yandex.ru>
2021-06-01 14:53:47 +0300

    Решение восемьдесят второй задачи из контрольных вопросов

---
+++ Control Questions/js/question82.js
@@ -0,0 +1,8 @@
+let calculateSqueare = function(number) {
+    return number ** 2;
+}
+
+console.log(calculateSqueare(5));
+
+/* Напиши функцию, которая принимает в качестве параметра число, и возвращает квадрат
+этого числа. */
