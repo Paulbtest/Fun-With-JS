97218a4af80f67b3dcba5a4d27c8394557705705
PaulBTest <buchkovpv@yandex.ru>
2021-06-05 16:12:38 +0300

    Создана папка для упрежнений с сайта Javascript.info

---
+++ Javascript info/2.4 Variables/js/workingWithVariables.js
@@ -0,0 +1,10 @@
+let name = "John";
+let admin = name;
+
+alert(admin);
+
+
+/* Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert(must output “John”). */
