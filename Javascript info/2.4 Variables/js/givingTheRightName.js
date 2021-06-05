b2434aea38dc1c63960cdb753fdd2508efa05e3b
PaulBTest <buchkovpv@yandex.ru>
2021-06-05 16:17:55 +0300

    Решение задачи "Giving the right name" с сайта Javascript.info

---
+++ Javascript info/2.4 Variables/js/givingTheRightName.js
@@ -0,0 +1,8 @@
+let ourPlanetName = "Earth";
+let currentUserName = "Stranger";
+
+console.log(ourPlanetName);
+console.log(currentUserName);
+
+/* Create a variable with the name of our planet.How would you name such a variable ?
+Create a variable to store the name of a current visitor to a website.How would you name that variable ? */
